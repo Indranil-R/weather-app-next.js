0f151bd4c5a95399b8d7757672cd1af6ed514fd6
Indranil Roy <87275536+Indranil-R@users.noreply.github.com>
2023-03-26 02:01:03 +0530

    Weather App

--- pages/components/Weather.js
+++ pages/components/Weather.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Weather = () => {
@@ -5,34 +6,49 @@ const Weather = () => {
   const [city, setCity] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Your city name is: ${city}`);
+    if (city !== "") {
+      fetchWeather(city);
+    }
   };
-
+  const fetchWeather = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`;
+    const response = await axios.get(url);
+    setWeatherData(response.data);
+    console.log(weatherData);
+    // console.log(weatherData.main.temp)
+  };
   return (
-    <div class="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-      <h1 class="mb-8 text-4xl font-bold">Weather App</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+      <h1 className="mb-8 text-4xl font-bold">Weather App</h1>
       <form
-        class="mb-8 flex flex-col sm:flex-row items-center sm:space-x-4"
+        className="mb-8 flex flex-col sm:flex-row items-center sm:space-x-4"
         onSubmit={handleSubmit}
       >
         <input
-          class="border border-green-500 rounded-md px-4 py-2 mb-4 sm:mb-0 w-full sm:w-auto focus:outline-none focus:ring-2 focus:ring-green-500"
+          className="border border-green-500 rounded-md px-4 py-2 mb-4 sm:mb-0 w-full sm:w-auto focus:outline-none focus:ring-2 focus:ring-green-500"
           type="text"
           value={city}
           onChange={(e) => setCity(e.target.value)}
           placeholder="Enter city name"
         />
         <button
-          class="rounded-md bg-green-500 px-4 py-2 font-bold text-white shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+          className="rounded-md bg-green-500 px-4 py-2 font-bold text-white shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
           type="submit"
         >
           Get Weather
         </button>
       </form>
       {weatherData && (
-        <div class="w-full max-w-xl rounded-lg bg-white p-8 shadow-md">
-          <p class="mb-4 text-5xl font-bold text-green-500"></p>
-          <p class="text-lg font-medium text-gray-500"></p>
+        <div className="w-full max-w-xl rounded-lg bg-white p-8 shadow-md">
+          <h1 className="text-2xl font-bold mb-4">
+            Weather for {weatherData.name}, {weatherData.sys.country}
+          </h1>
+          <p className="mb-4 text-5xl font-bold text-green-500">
+            {Math.round(weatherData.main.temp - 273.15)}°C
+          </p>
+          <p className="text-lg font-medium text-gray-500">
+            {weatherData.weather[0].description}
+          </p>
         </div>
       )}
     </div>
